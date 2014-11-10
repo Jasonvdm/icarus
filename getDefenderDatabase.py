7a19399c7b1c0b173b7b517c0e46fb010b4b1e96
bridge-eimon <beimon@stanford.edu>
2014-11-10 15:28:51 -0800

    added defender csv

---
+++ getDefenderDatabase.py
@@ -0,0 +1,21 @@
+import csv
+
+sentiment_file = open("SoccerStats.csv", 'rU')
+reader = csv.reader(sentiment_file)
+index = 0
+for row in reader:
+	if index == 0: 
+		print row
+		keys = row
+	index+=1
+reader = csv.DictReader(sentiment_file)
+sentiment_file.seek(0)
+with open('Defenders.csv', 'wb') as test_file:
+	index = 0;
+	for row in reader:
+		if index == 0: 
+			file_writer = csv.DictWriter(test_file, keys)
+			file_writer.writeheader()
+		if row['Position Id'] == "2": file_writer.writerow(row)
+		index +=1
+test_file.close()
