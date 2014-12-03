1e0b8e17c0c3b35761a11c7bb77d1a7b63161e95
bridge-eimon <beimon@stanford.edu>
2014-12-03 11:02:11 -0800

    pushing current code

--- learnWeights.py
+++ learnWeights.py
@@ -1,5 +1,6 @@
 import numpy
 import matplotlib.pyplot as plt
+import csv
 
 from sklearn import linear_model
 from sklearn import datasets
@@ -10,6 +11,41 @@ featureMat = numpy.loadtxt('Data/DefendersFeatureMatrixMin.csv',delimiter = ",")
 labels = numpy.loadtxt('Data/DefendersSalaryMin.csv',delimiter =",")
 headers = numpy.loadtxt('Data/HeadersMin.csv', dtype = str , delimiter=",")
 
+teams = ["Arsenal", "Aston Villa", "Blackburn Rovers", "Bolton Wanderers", "Chelsea", "Everton", "Fulham", "Liverpool", "Manchester City", "Manchester United", "Newcastle United", "Norwich City", "Queens Park Rangers", "Stoke City", "Sunderland", "Swansea City", "Tottenham Hotspur", "West Bromwich Albion", "Wigan Athletic", "Wolverhampton Wanderers"]
+
+
+sentiment_file = open("Data/SoccerStats.csv", 'rU')
+reader = csv.reader(sentiment_file)
+index = 0
+for row in reader:
+	if index == 0: 
+		keys = row
+		break
+
+
+reader = csv.DictReader(sentiment_file)
+sentiment_file.seek(0)
+
+goalsScoredAgainst = dict()
+
+with open('Data/DefendersAvg.csv', 'wb') as test_file:
+	index = 0
+	for row in reader:
+		opposition = row['Opposition']
+		if opposition in goalsScoredAgainst:
+			goalsScoredAgainst[opposition] += int(row['Goals'])
+		else:
+			goalsScoredAgainst[opposition] = int(row['Goals'])
+labels = []
+for team in teams:
+	if team != "Arsenal":
+		labels.append(goalsScoredAgainst[team])
+	labels.append(goalsScoredAgainst[team])
+	labels.append(goalsScoredAgainst[team])
+	labels.append(goalsScoredAgainst[team])
+
+
+
 # pca = PCA(n_components = 2)
 # featureMat = pca.fit(featureMat).transform(featureMat)
 
@@ -19,9 +55,9 @@ headers = numpy.loadtxt('Data/HeadersMin.csv', dtype = str , delimiter=",")
 # plt.legend()
 # plt.show()
 
-trainMat = featureMat[20:]
+trainMat = featureMat[:]
 testMat = featureMat[:20]
-trainLabels = labels[20:]
+trainLabels = labels[:]
 testLabels = labels[:20]
 
 #model = linear_model.LinearRegression()
@@ -37,6 +73,6 @@ for index in range(len(predictions)):
 	print str(testLabels[index])+": "+str(predictions[index])
 
 for index in range(len(headers)):
- 	print headers[index] +": "+str(model.coef_[index])
+ 	print headers[index] +": "+str(model.coef_[index]*-1)
 
 
